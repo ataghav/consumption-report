b91b494e0fdc4a50bb2e61e4c6a0147f79aa1705
ataghav <ataghav@gmail.com>
2020-02-26 16:21:08 +0100

    init

---
+++ src/main/java/com/zenhomes/consumptionreport/ConsumptionReportApplication.java
@@ -0,0 +1,13 @@
+package com.zenhomes.consumptionreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumptionReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumptionReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zenhomes/consumptionreport/ConsumptionReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenhomes.consumptionreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumptionReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
