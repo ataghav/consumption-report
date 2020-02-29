dc93cd01f388d0c3aab25bb6ee828381b96b99d2
ataghav <ataghav@gmail.com>
2020-02-29 17:25:49 +0100

    Redundant comments removed.

--- src/test/java/com/zenhomes/consumptionreport/services/ReadingServiceImplTest.java
+++ src/test/java/com/zenhomes/consumptionreport/services/ReadingServiceImplTest.java
@@ -12,34 +12,5 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @SpringBootTest
 @ContextConfiguration(classes = {ExternalServicesRepository.class, ReadingRepository.class, VillageRepository.class})
 public class ReadingServiceImplTest {
-//    @Test
-//    void newReading() {
-//    }
 
-//    @MockBean
-//    ExternalServicesRepository externalServicesRepository;
-//
-//    @MockBean
-//    ReadingRepository readingRepository;
-//
-//    @MockBean
-//    VillageRepository villageRepository;
-//
-//    private ReadingServiceImpl service = new ReadingServiceImpl(externalServicesRepository,
-//            readingRepository, villageRepository);
-//
-//    @Test
-//    public void newReadingMakesExternalServiceCall() {
-//        Mockito.when(externalServicesRepository.lookupVillage(1)).thenReturn(new VillageInfoDTO(1,new VillageLookupDTO(100,"Villarriba")));
-//        Mockito.when(villageRepository.existsById(1)).thenReturn(false);
-//
-//        service.newReading(new CallBackDTO(1,100.123));
-//
-//        Mockito.verify(villageRepository, Mockito.times(1)).save(new Village());
-//    }
-//
-//    @Test
-//    public void newReadingPersistsNewVillages() {
-//
-//    }
 }
